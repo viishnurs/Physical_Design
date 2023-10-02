7681d27385fc3017c490d4718c6c99009ccc1231
vishnu rs <85409531+viishnurs@users.noreply.github.com>
2023-10-02 22:15:03 +0530

    Create boundary_tap.tcl

---
+++ PD_ADV/ORCA_TOP/PD/scripts/boundary_tap.tcl
@@ -0,0 +1,16 @@
+### placement of boundary cells
+
+set_boundary_cell_rules -left_boundary_cell saed32_hvt|saed32_hvt_std/DCAP_HVT -right_boundary_cell saed32_hvt|saed32_hvt_std/DCAP_HVT -at_va_boundary
+
+compile_boundary_cells
+
+check_boundary_cells
+
+
+#####   placement of TAP cells 
+
+create_tap_cells -lib_cell saed32_hvt|saed32_hvt_std/DCAP_HVT -distance 30 -pattern stagger -skip_fixed_cells
+check_leagality
+
+
+save_block -as boundary_tap_placement
