36e0b7b3bd53e2290ac4ed481e881dc493baeb6e
vishnu rs <85409531+viishnurs@users.noreply.github.com>
2023-10-05 21:54:20 +0530

    Update 3powerplan.tcl

--- PD_ADV/ORCA_TOP/PD/scripts/3powerplan.tcl
+++ PD_ADV/ORCA_TOP/PD/scripts/3powerplan.tcl
@@ -1,1 +1,13 @@
-code for powerplanning 
+###code for powerplanning 
+###### VISHNU RS########################
+########################################
+
+connect_pg_net 
+
+create_pg_ring_pattern VDD_VSS_RING -hotizontal_layers M7 -horizontal_width 0.112 -vertical_layers M8 -vertical_width 0.112 -nets {VDD VSS}
+
+set_pg_strategy VDD_VSS_RING_STARATEGY -pattern {{name: VDD_VSS_RING } {nets: VDD VSS} } -core
+
+complie_pg_startegies {VDD_VSS_RING_STARATEGY}
+
+source ./scripts/powerfinal.tcl
