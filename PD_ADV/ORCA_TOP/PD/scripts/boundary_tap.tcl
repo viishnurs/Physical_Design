9a5e162ff977746e291ee070e67fc55db1773dd3
vishnu rs <85409531+viishnurs@users.noreply.github.com>
2023-11-03 11:08:50 +0530

    Update boundary_tap.tcl

--- PD_ADV/ORCA_TOP/PD/scripts/boundary_tap.tcl
+++ PD_ADV/ORCA_TOP/PD/scripts/boundary_tap.tcl
@@ -1,9 +1,14 @@
-#### voltage area creation 
-source ./scripts/vol_area_creation.tcl
-va_create PD_RISC_CORS pb_0 5.016
+
+#source ./scripts/vol_area_creation.tcl
+#va_create PD_RISC_CORE pb_0 3.344
 
 
 ### placement of boundary cells
+
+create_keepout_margin -type hard -outer {2 2 2 2 } [get_flat_cells -filter "is_hard_macro"]
+
+remove_cells [get_flat_cells *boundary* -all ]
+remove_cells [get_flat_cells *tapfiller* -all ]
 
 set_boundary_cell_rules -left_boundary_cell saed32_hvt|saed32_hvt_std/DCAP_HVT -right_boundary_cell saed32_hvt|saed32_hvt_std/DCAP_HVT -at_va_boundary
 
@@ -15,14 +20,12 @@ check_boundary_cells
 #####   placement of TAP cells 
 
 create_tap_cells -lib_cell saed32_hvt|saed32_hvt_std/DCAP_HVT -distance 30 -pattern stagger -skip_fixed_cells
-check_leagality
+check_legality
 
 
-save_block -as boundary_tap_placement
-
-
-### SANITY CHECKS 
+#save_block -as boundary_tap_placement
+save_block -as floor_plan_main
+######## SANITY CHECKS #########33
 
 check_mv_design
-create_utilization -configration -include all fp
-report_utilization -config fp
+report_utilization
