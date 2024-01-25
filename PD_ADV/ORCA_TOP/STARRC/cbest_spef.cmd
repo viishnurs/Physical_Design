0ecd64152c81721cceade9e7a9b899d43aa75af8
vishnu rs <85409531+viishnurs@users.noreply.github.com>
2024-01-25 10:08:28 +0530

    Create cbest_spef.cmd

---
+++ PD_ADV/ORCA_TOP/STARRC/cbest_spef.cmd
@@ -0,0 +1,9 @@
+TCAD_GRD_FILE: /home/vishnu/PD_Adv_Aug23/ORCA_TOP/STARRC/inputs/saed32nm_1p9m_Cmin.nxtgrd
+MAPPING_FILE: /home/vishnu/PD_Adv_Aug23/ORCA_TOP/STARRC/inputs/saed32nm_tf_itf_tluplus.map
+NETLIST_FORMAT: SPEF
+BLOCK : route_opt_done
+NDM_DATABASE: /home/vishnu/PD_Adv_Aug23/ORCA_TOP/PD/outputs/work/ORCA_TOP.nlib
+OPERATING_TEMPERATURE: -40
+SELECTED_CORNERS: Cmin
+NETLIST_FILE: /home/vishnu/PD_Adv_Aug23/ORCA_TOP/STARRC/outputs/SPEF/Cmin_m40.spef
+COUPLE_TO_GROUND: NO
